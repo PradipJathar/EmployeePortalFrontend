675f0b46acd39cbb1e41a4509477171d0e98446e
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-10-13 22:59:09 +0530

    Add get employee service.

--- src/app/Services/employee.service.ts
+++ src/app/Services/employee.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Employee } from '../Models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  private apiUrl = 'https://localhost:7021/api/Employee';
+
   constructor() { }
+
+  http = inject(HttpClient)
+
+  getAllEmployee()
+  {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
 }
