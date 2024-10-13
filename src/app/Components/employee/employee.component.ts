b823b40016c911dc455f408f56e064133c79aa06
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-10-13 22:22:57 +0530

    Empolyee component form binding.

--- src/app/Components/employee/employee.component.ts
+++ src/app/Components/employee/employee.component.ts
@@ -1,15 +1,20 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
   @ViewChild("myModal") model : ElementRef | undefined;
   
+    employeeForm : FormGroup = new FormGroup({});
+
+    constructor(private fb: FormBuilder){};
+
     openModel()
     {
       const empModel = document.getElementById("myModal");
@@ -28,4 +33,24 @@ export class EmployeeComponent {
       }
     }
   
-  }
+    setFormState()
+    {
+      this.employeeForm = this.fb.group({
+
+        id: [0],
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        mobile: ['', Validators.required],
+        age: ['', Validators.required],
+        salery: ['', Validators.required],
+        status: [false, Validators.required]
+
+      });
+    }    
+
+    onSubmit()
+    {
+      console.log(this.employeeForm.value);
+    }
+
+}
